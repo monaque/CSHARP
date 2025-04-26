d72d598d601dc67ff8a3778e1a83b692ec18d7ce
monaque <valeriy.drevko@gmail.com>
2025-04-26 19:02:01 +0400

    Switch to another PC

--- Lesson1/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ Lesson1/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbdf49bb1b97c8a6ab505a8b880c21f204fe8ea8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e3cc9a630da69d01006971994533ac0976fb978")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
