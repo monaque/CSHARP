86458232c7490df2f31890a749cd6f8b0cb5e358
monaque <valeriy.drevko@gmail.com>
2025-04-26 18:34:10 +0400

    Changes in Task1

--- CSharp_seminar1/Task1/Program.cs
+++ CSharp_seminar1/Task1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num2 * num2 == num1)
+{
+    Console.WriteLine("Первое число является квадратом второго.");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго.");
+}
